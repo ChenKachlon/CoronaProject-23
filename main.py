0c12630e6534fe2a8c45e55060d67b1b6a10f779
eli9914 <eli9914@gmail.com>
2020-11-30 22:35:35 +0200

    Update main.py
    adding release patient
    adding amount of beds in given department

--- main.py
+++ main.py
@@ -27,9 +27,37 @@ def add_patient():
     for i in range(4):
         ws[ConvertToStr(i+1,row_location)] = data[i]
 
+def release_patient():
+    """Release patients from the data base"""
+    row_location = Get_Row_Indicator(ws.max_row)
+    release_id = input("Please enter the id of the patient you want to release: ")
+    flag = 0
+    for i in range(2,row_location):
+        if str(ws.cell(row=i, column=1).value) == str(release_id):
+            ws.delete_rows(i)
+            flag = 1
+    if flag == 0:
+        print("Sorry, Id doesn't exists in the data base")
+    else:
+        print("Patient released")
+
+def amount_of_beds_in_given_department():
+    """Calculate the amount of beds in given department"""
+    row_location_beds = Get_Row_Indicator(ws_beds.max_row)
+    amount = 0
+    department_name = input("Please enter department name: ")
+    for i in range(3,row_location_beds):
+        if str(ws_beds.cell(row=i,column=1).value) == str(department_name):
+            amount += 1
+    if amount == 0:
+        print("Department doesn't exist")
+        exit(1)
+    print("Amount of beds in {0} department is: {1}".format(department_name,amount))
 
 def main():
     add_patient()
+    release_patient()
+    amount_of_beds_in_given_department()
     Save_Data()
 
 main()
