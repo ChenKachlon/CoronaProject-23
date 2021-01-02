316a1e78ba068860a6fbb3b4c4dcc3d5d76a1002
ChenKachlon <73073301+ChenKachlon@users.noreply.github.com>
2021-01-02 12:59:49 +0200

    linked navbar

--- TheSiteCorona23/accounts/urls.py
+++ TheSiteCorona23/accounts/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home),
-    path('patients/', views.patients),
-    path('beds/', views.beds),
-    path('ventilators/', views.ventilators),
+    path('', views.home, name='home'),
+    path('patients/', views.patients, name='patients'),
+    path('beds/', views.beds, name='beds'),
+    path('ventilators/', views.ventilators, name='ventilators'),
 
 ]
