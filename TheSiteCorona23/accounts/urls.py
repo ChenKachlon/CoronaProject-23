cd8cdad18f0c98c81199c670a4db20f9b9c02b8e
Matan Ben Ishay <matan12333445@gmail.com>
2021-01-05 10:45:56 +0200

    lilttlr change

--- TheSiteCorona23/accounts/urls.py
+++ TheSiteCorona23/accounts/urls.py
@@ -4,6 +4,9 @@ from . import views
 # app_name = 'accounts'
 
 urlpatterns = [
+    path('register/', views.registerPage, name='register'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
     path('', views.home, name='home'),
     path('patients/', views.patients, name='patients'),
     path('beds/', views.beds, name='beds'),
@@ -11,4 +14,6 @@ urlpatterns = [
     path('add_patient/', views.addPatients, name='add_patient'),
     path('add_bed/', views.addBeds, name='add_bed'),
     path('add_ven/', views.addVen, name='add_ven'),
+    # path('staff/<str:pk>/', views.staff, name='staff_page'),
+
 ]
