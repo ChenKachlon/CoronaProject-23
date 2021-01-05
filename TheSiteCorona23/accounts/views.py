f63b08da623d73e05abf6e59cc8e3e75979b3d23
ChenKachlon <73073301+ChenKachlon@users.noreply.github.com>
2021-01-05 05:03:45 +0200

    add login,logout & register need to continue (permissions)

--- TheSiteCorona23/accounts/views.py
+++ TheSiteCorona23/accounts/views.py
@@ -1,8 +1,53 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import *
 from .forms import *
+
+# Create your views here.
+
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for ' + user)
+                return redirect('login')
+        context = {'form':form}
+        return render(request,'accounts/register.html',context)
+
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Incorrect username OR password')
+        context = {}
+        return render(request, 'accounts/login.html', context)
+
+
+@login_required(login_url='login')
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
 def home(request):
     pati=Patient.objects.all()
     bedi=Bed.objects.all()
@@ -19,18 +64,22 @@ def home(request):
 
     return render(request,'accounts/dashboard.html',context)
 
+@login_required(login_url='login')
 def patients(request):
-    pat=Patient.objects.all()
-    return render(request,'accounts/patients.html',{'patients':pat})
+    pat = Patient.objects.all()
+    return render(request, 'accounts/patients.html', {'patients':pat})
 
+@login_required(login_url='login')
 def beds(request) :
     bedd = Bed.objects.all()
     return render(request,'accounts/beds.html',{'beds': bedd })
 
+@login_required(login_url='login')
 def ventilators(request) :
     venn = Ventilator.objects.all()
     return render(request,'accounts/Ventilators.html',{'Ventilators': venn})
 
+@login_required(login_url='login')
 def addPatients(request):
     form=PatientForm()
     if request.method=='POST':
@@ -41,6 +90,7 @@ def addPatients(request):
     context={'form':form}
     return render(request,'accounts/patients_form.html',context)
 
+@login_required(login_url='login')
 def addBeds(request):
     form=BedForm()
     if request.method=='POST':
@@ -51,6 +101,7 @@ def addBeds(request):
     context={'form':form}
     return render(request,'accounts/beds_form.html',context)
 
+@login_required(login_url='login')
 def addVen(request):
     form=VenForm()
     if request.method=='POST':
@@ -60,3 +111,5 @@ def addVen(request):
             return redirect('/')
     context={'form':form}
     return render(request,'accounts/ventilators_form.html',context)
+
+# def staff(request):
